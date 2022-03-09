8b87755ea55876fcc17c95cc591d948c6b2009c5
yogesh-karki <yogesh@hueshine.com>
2022-03-09 17:28:41 +0545

    click data filter

--- src/pages/Portal/Portal.jsx
+++ src/pages/Portal/Portal.jsx
@@ -11,7 +11,14 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import ProvinceLabel from "./data/ProvinceLabel.geojson";
 import ProvinceMap from "./data/ProvinceMap.json";
 
-import RecyclerSaathiData from "./data/mainData/AllData.geojson";
+import recyclerSaathiData from "./data/mainData/AllData.geojson";
+
+
+
+
+import {navData, dataStyle, provinceLabelStyle ,mapStyleLine} from './PortalStyle'
+
+import { CloseCircleOutlined } from '@ant-design/icons';
 
 import "./portal.scss";
 
@@ -29,138 +36,67 @@ const Portal = () => {
         maxZoom: 20,
     });
 
-    const [mapData, setMapData] = useState(RecyclerSaathiData);
+
+
+
 
     const MAPBOX_TOKEN = "pk.eyJ1IjoieW9nZXNoa2Fya2kiLCJhIjoiY2txZXphNHNlMGNybDJ1cXVmeXFiZzB1eSJ9.A7dJUR4ppKJDKWZypF_0lA";
 
-    const provinceLabelStyle = {
-        id: "provinceLabel",
-        type: "symbol",
-        layout: {
-            "text-allow-overlap": false,
-            "text-field": ["get", "description"],
-            "text-variable-anchor": ["top", "bottom", "left", "right"],
-            "text-radial-offset": 0.5,
-            "text-justify": "auto",
-            "text-size": 12,
-            "text-font": ["Open Sans Bold", "Arial Unicode MS Bold"],
-        },
-        paint: {
-            "text-color": "#0a405a",
-        },
-    };
-
-    const mapStyleLine = {
-        id: "map_style",
-        type: "line",
-        layout: {},
-        paint: {
-            "line-color": "#0a405a",
-            "line-width": 1.5,
-            "line-dasharray": [2, 1],
-        },
-    };
-
-    const [filterData, setFilterData] = useState();
-
-    const navData = [
-      
-        {
-            num: "10",
-            title: "Waste Smart Schools",
-            subtitle: "schools converted to",
-            color: "#219F94"
-        },
-        {
-            num: "17",
-            title: "Organisational Network",
-            subtitle: "working with us",
-            color: "#630606"
-        },
-        {
-            num: "06",
-            title: "Volunteer For Change",
-            subtitle: "volunteer available",
-            color: '#573391'
-        },
-        {
-            num: "27",
-            title: "Collection Partners",
-            subtitle: "working with us",
-            color: '#EF6D6D'
-        },
-        {
-            num: "01",
-            title: "Regional Office",
-            subtitle: "situated at",
-            color: '#10b5c9'
-        },
-        {
-            num: "01",
-            title: "Waste Smart Musuem",
-            subtitle: "Waste Smart Musuem",
-            color: "red"
-        },
-
-        {
-            num: "02",
-            title: "PET recycling center",
-            subtitle: "PET recycling center",
-            color: "#D9CE3F"
-        },
-    ];
-
-    const dataStyle = {
-        id: "dataStyle",
-        type: "circle",
-        // filter: ['==', 'title', `Organisational Network`],
-        paint: {
-            "circle-radius": 10,
-            "circle-stroke-width": 2,
-            "circle-color": [
-                "match",
-                ["get", "title"],
-                "Organisational Network",
-                "#630606",
-                "PET recycling center",
-                "#D9CE3F",
-                "Regional Office",
-                "#10b5c9",
-                "Volunteer For Change",
-                "#573391",
-                "Collection Partners",
-                "#EF6D6D ",
-                "Waste Smart Schools",
-                "#219F94",
-                "Waste Smart Musuem",
-                "red",
-                /* other */ "#BD1E1E",
-            ],
-        },
-    };
-
-  
+    
     const [isExpand , setIsExpand] = useState(-1)  
+    const [filterData , setFilterData] = useState(null)  
 
-    let cardActive = () => {
-      console.log("hello")
+    const navClick = (index, title) => {
+      setIsExpand(index)
+      setFilterData(title)
     }
+
+
+
+    const dataStyleFilter = {
+      id: "dataStyleFilter",
+      type: "circle",
+      filter: ['==', 'title', `${filterData}`],
+      paint: {
+          "circle-radius": 10,
+          "circle-stroke-width": 2,
+          "circle-color": [
+              "match",
+              ["get", "title"],
+              "Organisational Network",
+              "#EA5C2B",
+              "PET recycling center",
+              "#D9CE3F",
+              "Regional Office",
+              "#10b5c9",
+              "Volunteer For Change",
+              "#573391",
+              "Collection Partners",
+              "#EF6D6D ",
+              "Waste Smart Schools",
+              "#219F94",
+              "Waste Smart Musuem",
+              "red",
+              /* other */ "#BD1E1E",
+          ],
+      },
+  };
 
     return (
         <>
             <div className="portal">
-             
 
                 <div className="legend_click">
                     {
                       navData.map((val, index) => {
                         return (
-                            <div className={`${isExpand === index ? 'expand' : ''} card `}  key={index} onClick={() => setIsExpand(index)}>
+                            <div className={`${isExpand === index ? 'expand' : ''} card `}  key={index} onClick={() => navClick(index,val.title) }>
                                 <div className="num">
                                     <h3>{val.num}</h3>
                                 </div>
+
                                 <div className="desc">
-                                    {/* <h5>{val.subtitle}</h5> */}
+                                  
                                     <h4>{val.title}</h4>
                                 </div>
                                 <span className="border" style={{backgroundColor: val.color}} ></span>
@@ -178,9 +114,15 @@ const Portal = () => {
                         <Layer {...mapStyleLine} />
                     </Source>
 
-                    <Source id="vfcData" type="geojson" data={mapData}>
-                        <Layer {...dataStyle} />
+                    <Source id="allData" type="geojson" data={recyclerSaathiData}>
+                      {
+                        filterData ? <Layer id="layer1" {...dataStyleFilter} /> : <Layer id="layer2" {...dataStyle} />
+                      }
                     </Source>
+
+                 
+
+          
                 </Map>
             </div>
         </>
---
+++ src/pages/Portal/PortalStyle.js
@@ -0,0 +1,104 @@
+const navData = [
+      
+    {
+        num: "10",
+        title: "Waste Smart Schools",
+        subtitle: "schools converted to",
+        color: "#219F94"
+    },
+    {
+        num: "17",
+        title: "Organisational Network",
+        subtitle: "working with us",
+        color: "#EA5C2B"
+    },
+    {
+        num: "06",
+        title: "Volunteer For Change",
+        subtitle: "volunteer available",
+        color: '#573391'
+    },
+    {
+        num: "27",
+        title: "Collection Partners",
+        subtitle: "working with us",
+        color: '#EF6D6D'
+    },
+    {
+        num: "01",
+        title: "Regional Office",
+        subtitle: "situated at",
+        color: '#10b5c9'
+    },
+    {
+        num: "01",
+        title: "Waste Smart Musuem",
+        subtitle: "Waste Smart Musuem",
+        color: "red"
+    },
+
+    {
+        num: "02",
+        title: "PET recycling center",
+        subtitle: "PET recycling center",
+        color: "#D9CE3F"
+    },
+];
+
+const dataStyle = {
+    id: "dataStyle",
+    type: "circle",
+    paint: {
+        "circle-radius": 10,
+        "circle-stroke-width": 2,
+        "circle-color": [
+            "match",
+            ["get", "title"],
+            "Organisational Network",
+            "#EA5C2B",
+            "PET recycling center",
+            "#D9CE3F",
+            "Regional Office",
+            "#10b5c9",
+            "Volunteer For Change",
+            "#573391",
+            "Collection Partners",
+            "#EF6D6D ",
+            "Waste Smart Schools",
+            "#219F94",
+            "Waste Smart Musuem",
+            "red",
+            /* other */ "#BD1E1E",
+        ],
+    },
+};
+
+const provinceLabelStyle = {
+    id: "provinceLabel",
+    type: "symbol",
+    layout: {
+        "text-allow-overlap": false,
+        "text-field": ["get", "description"],
+        "text-variable-anchor": ["top", "bottom", "left", "right"],
+        "text-radial-offset": 0.5,
+        "text-justify": "auto",
+        "text-size": 12,
+        "text-font": ["Open Sans Bold", "Arial Unicode MS Bold"],
+    },
+    paint: {
+        "text-color": "#0a405a",
+    },
+};
+
+const mapStyleLine = {
+    id: "map_style",
+    type: "line",
+    layout: {},
+    paint: {
+        "line-color": "#0a405a",
+        "line-width": 1.5,
+        "line-dasharray": [2, 1],
+    },
+};
+
+export {navData, dataStyle, provinceLabelStyle ,mapStyleLine}
